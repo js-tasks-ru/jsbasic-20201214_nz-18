8c581b956c7231e460c529ce617cdc3a15763201
Natalya Zernova <nz_18@mail.ru>
2021-01-24 22:56:55 +0300

    add function sumSalary(salaries)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,12 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
+let sumSalar;
+let sumSalarFiltred;
+let result; 
+
 function sumSalary(salaries) {
-  // ваш код...
-}
+  sumSalar = Object.values(salaries);
+  sumSalarFiltred = sumSalar.filter(item => {if (isFinite(item)) {return item;}});
+  result = sumSalarFiltred.reduce((sum, current) => sum + current, 0);
+  return result;}
