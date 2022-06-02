c3ac1809b7d446d1d64adcd5cbeef79c8e9987e6
Nataly Zernova <nz_18@mail.ru>
2022-06-02 14:19:12 +0300

    sovle tasks 3.4 and 3.5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -3,6 +3,11 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
-  // ваш код...
-}
+
+const getMinMax = (str) => {
+  const numbers = str.split(' ').filter(item => parseFloat(item)).map(item => parseFloat(item.replace(/,/g, '')));
+  return {
+    min: Math.min(...numbers),
+    max:Math.max(...numbers)
+  }
+};
